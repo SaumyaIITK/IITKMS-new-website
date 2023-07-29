329f9a08ba01563deef33c61d9035499e6938abd
SaumyaIITK <107754396+SaumyaIITK@users.noreply.github.com>
2023-07-29 09:20:53 +0530

    files added

--- js/overall.js
+++ js/overall.js
@@ -1,79 +1,92 @@
 const toggleNav = () => {
-    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
-  }
+  document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
+};
 
 const sections = document.getElementsByName('main');
 
 const sectBtns = document.getElementById('nav-links');
-const sectBtn = document.querySelectorAll('.nav-link')
+const sectBtn = document.querySelectorAll('.nav-link');
 
-
-const allSections = document.querySelectorAll('.main-content')
+const allSections = document.querySelectorAll('.main-content');
 console.log(sectBtns);
-function PageTransitions(){
-    //Sections Activator
-    sectBtns.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;
-        if(id){
-            document.body.dataset.nav = document.body.dataset.nav = "false";
-            //hide other sections
-            sections.forEach((section)=>{
-                section.classList.remove('active')
-                section.classList.remove('animateOnStartUpIn');
-                section.classList.remove('animateOnNavedIn')
-            })
 
-            const element = document.getElementById(id);
-            element.classList.add('active');
-            element.classList.add('animateOnNavedIn')
-        }
-    })
-     
-    
+function PageTransitions() {
+  // Sections Activator
+  sectBtns.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      document.body.dataset.nav = false;
+      // Hide other sections
+      sections.forEach((section) => {
+        section.classList.remove('active', 'animateOnStartUpIn', 'animateOnNavedIn');
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add('active', 'animateOnNavedIn');
+    }
+  });
 }
 
 const trailer = document.getElementById("trailer");
 
 const animateTrailer = (e, interacting) => {
-  const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
-  
+  const x = e.clientX - trailer.offsetWidth / 2;
+  const y = e.clientY - trailer.offsetHeight / 2;
+
   const keyframes = {
     transform: `translate(${x}px, ${y}px) scale(${interacting ? 2 : 1})`
-  }
-  
-  trailer.animate(keyframes, { 
-    duration: 100, 
-    fill: "forwards" 
-  });
-}
+  };
 
-const getTrailerClass = type => {
-  switch(type) {
+  trailer.animate(keyframes, {
+    duration: 70,
+    fill: "forwards"
+  });
+};
+
+const getTrailerClass = (type) => {
+  switch (type) {
     case "rotate":
-        return "fa-solid fa-arrows-rotate";
+      return "fa-solid fa-arrows-rotate";
     case "video":
       return "fa-solid fa-play";
     case "button":
-        return "fa-solid fa-circle-dot";
+      return "fa-solid fa-circle-dot";
     default:
-      return "fa-solid fa-up-right-from-square"; 
+      return "fa-solid fa-up-right-from-square";
   }
-}
+};
 
-window.onmousemove = e => {
-  const interactable = e.target.closest(".interactable"),
-        interacting = interactable !== null;
-  
+window.onmousemove = (e) => {
+  const interactable = e.target.closest(".interactable");
+  const interacting = interactable !== null;
+
   const icon = document.getElementById("trailer-icon");
-  
+
   animateTrailer(e, interacting);
-  
+
   trailer.dataset.type = interacting ? interactable.dataset.type : "";
-  
-  if(interacting) {
+
+  if (interacting) {
     icon.className = getTrailerClass(interactable.dataset.type);
   }
-}
+};
 
 PageTransitions();
+
+function Links() {
+  // Get all Anchor elements
+  var a = document.getElementsByTagName('a');
+  // Loop through each anchor element found.
+  for (var i = 0; i < a.length; i++) {
+    // Set onclick event for the anchor element
+    a[i].addEventListener('click', Anchors, false);
+  }
+}
+
+function Anchors(event) {
+  event.preventDefault(); // Prevent the default anchor click behavior
+  // Set new window location using the anchor href that triggers this function.
+  window.location.href = this.href;
+}
+
+window.onload = Links;
